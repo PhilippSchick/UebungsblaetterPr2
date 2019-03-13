caec906ce2e3dd49264fc64d239b1aee66556873
Philipp Schick <philipp.schick@student.kit.edu>
2019-03-13 13:12:43 +0100

    Generate new Project connectFour

---
+++ src/de/pschick/connectFour/Player.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package de.pschick.connectFour;
+
+/**
+ * @author Philipp Schick
+ * @version 1.00, 13.03.2019
+ *
+ */
+public interface Player {
+
+}
---
+++ src/de/pschick/connectFour/Starter.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package de.pschick.connectFour;
+
+/**
+ * @author Philipp Schick
+ * @version 1.00, 13.03.2019
+ *
+ */
+public class Starter {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
