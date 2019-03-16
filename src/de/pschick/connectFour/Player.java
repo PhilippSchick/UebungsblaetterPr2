fea26c1708fbdf8bf76fc6bc066860a63b60e591
philipp <philipp@philipp-System>
2019-03-16 21:21:33 +0100

    added moveBehavior()

--- src/de/pschick/connectFour/Player.java
+++ src/de/pschick/connectFour/Player.java
@@ -7,19 +7,24 @@ package de.pschick.connectFour;
  * @author Philipp Schick
  * @version 1.00, 13.03.2019
  *
- * The Blueprint for a Player
+ *          The Blueprint for a Player
  */
 public abstract class Player {
 	/**
 	 * The name of the player
 	 */
 	protected String playerName;
-	
+
 	/**
 	 * The Field to Play on
 	 */
 	protected Field field;
-	
+
+	/**
+	 * The number of the player
+	 */
+	protected Integer playerNumber;
+
 	/**
 	 * Returns the name of the player
 	 * 
@@ -28,11 +33,41 @@ public abstract class Player {
 	public String getPlayerName() {
 		return playerName;
 	}
-	
+
 	/**
-	 * The Player makes a move 
+	 * The Player makes a move. Throws a {@link IllegalStateException} if the player
+	 * number isn't set yet.
 	 */
-	public abstract void makeMove();
-	
-	
+	public void makeMove() {
+		if (playerNumber == null) {
+			throw new IllegalStateException("Player number isn't set yet!");
+		}
+		
+		moveBehavior();
+	}
+
+	/**
+	 * Defines the behavior of a player if he makes a move
+	 */
+	protected abstract void moveBehavior();
+
+	/**
+	 * Sets the number of the player. Attention! Throws a
+	 * {@link IllegalStateException} if the number is already set. Throws a
+	 * {@link IllegalArgumentException} if the argument isn't a '0' or a '1'
+	 * 
+	 * @param pNumber The player number
+	 */
+	public void setPlayerNumber(int pNumber) {
+		if (playerNumber == null) {
+			if (pNumber == 1 || pNumber == 0) {
+				this.playerNumber = pNumber;
+			} else {
+				throw new IllegalArgumentException("The player number has to be a '0' or a '1'!");
+			}
+		} else {
+			throw new IllegalStateException("Number already set!");
+		}
+	}
+
 }
